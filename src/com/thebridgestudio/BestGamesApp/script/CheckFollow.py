ee8704e44b1835c4f632c3c220b00dd4f0679a8a
Wang Jianfei <jfwang213@qq.com>
2013-02-02 01:09:19 +0800

    fix bug of check follow

--- src/com/thebridgestudio/BestGamesApp/script/CheckFollow.py
+++ src/com/thebridgestudio/BestGamesApp/script/CheckFollow.py
@@ -6,13 +6,18 @@ from AppValue import BGApp
 from Operation import Operation
 from FriendShip import FriendShip
 
+import time
+
 if __name__ == '__main__':
     ops = Operation.FetchSomeDayOps(Operation.FollowType, 1)
     print "all number %d" % len(ops)
     followNum = 0
-    client = APIClientV2(BGApp.app_key, BGApp.app_secret)
+    client = APIClient(BGApp.app_key, BGApp.app_secret)
     client.set_access_token(BGApp.dev_token, time.time() + 90 * 24 *3600)
     for op in ops:
-        if FriendShip.CheckFollow(op.uid, BGApp.uid):
+        if FriendShip.CheckFollow(client, op.uid, BGApp.dev_uid):
+            print op.uid, op.ts
             followNum += 1
+        else:
+            print "not follow:", op.uid, op.ts
     print "follow num %d" % followNum
