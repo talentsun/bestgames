f79598777a646c5d62c5f0dd7bc54bf7a501142d
Wang Jianfei <jfwang213@qq.com>
2013-03-19 00:20:07 +0800

    去掉src目录

--- src/com/thebridgestudio/FollowApi/AccessInfo/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Access(models.Model):
-    uid = models.BigIntegerField(db_index=True, unique=True)
-    version = models.IntegerField()
-    data1 = models.CharField(max_length=50, default="")
-    data2 = models.CharField(max_length=50, default="")
--- src/com/thebridgestudio/FollowApi/resyncdb.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-mysql -uroot -pnameLR9969 -e "drop database if exists follow_api; create database follow_api character set = utf8;"
