6729b3c42225a4d52af47dc2ed06857bee8ee654
Qiao Sun <sunqiao@wandoujia.com>
2013-03-25 13:51:37 +0800

    MOD: change token;

--- weibo/src/com/thebridgestudio/FollowApi/AppValue.py
+++ weibo/src/com/thebridgestudio/FollowApi/AppValue.py
@@ -2,7 +2,7 @@
 
 class BGApp:
     dev_uid = 3177187433 
-    other_token = "2.00baJBTDycMMdCd14db81f370tubtd"
+    other_token = "2.00baJBTD06XASO35dcd07da0NOAOTC"
     dev_token = "2.00baJBTDKx5rQBd8e14e2c66kl7ixD"
     app_key = "1165281516"
     app_secret = "4360e65b0e9de717dfe3a0c127bc96b3"
