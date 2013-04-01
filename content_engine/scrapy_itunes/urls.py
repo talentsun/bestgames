6ec94719708270d5be4fa0d5629eb574458d6139
jianfeng <tujf.cn@gmail.com>
2013-04-01 15:45:57 +0800

    change url from getPic to tools/url2icon

--- content_engine/scrapy_itunes/urls.py
+++ content_engine/scrapy_itunes/urls.py
@@ -3,6 +3,6 @@ __author__ = 'huwei'
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('scrapy_itunes.views',
-    url(r'^getPic','getPic'),
+    url(r'^tools/url2icon','getPic'),
     url(r'^pic/(?P<filename>\w+\.jpg)','previewImage'),
 )
