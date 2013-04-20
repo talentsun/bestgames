da106e2a6058944bb4c29ba5d01d6ec0b6f5dd13
Hu Wei <huwei@wandoujia.com>
2013-04-20 17:48:00 +0800

    fix bug for hint

--- content_engine/portal/tables.py
+++ content_engine/portal/tables.py
@@ -76,7 +76,7 @@ class WeixinTable(tables.Table):
     id = tables.Column(orderable=False, visible=False)
     title = tables.Column(orderable=False)
     presenter = tables.Column(orderable=False)
-    weibo_sync_timestamp = DateTimeColumn(verbose_name=u"微博同步时间",orderable=True)
+    weibo_sync_timestamp = DateTimeColumn(verbose_name=u"微信同步时间",orderable=True)
     status = TemplateColumn(template_name="sync_status_field.html",orderable=False,verbose_name=u"同步状态")
     tags = TagColumn(orderable=False,attrs={"class":"tags"})
     ops = TemplateColumn(template_name="weixin_field_ops.html",verbose_name=u"操作",orderable=False,attrs={"class":"ops"})
